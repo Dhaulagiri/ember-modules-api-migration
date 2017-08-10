9ed47b643b53dfbdae9c31a5064de21acf10d580
Brian Runnells <brian.runnells@gmail.com>
2017-08-10 15:33:02 -0500

    ember-cli-shims import - after

--- app/components/component-two.js
+++ app/components/component-two.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
-import computed from 'ember-computed';
-import service from 'ember-service/inject';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
   store: service(),
